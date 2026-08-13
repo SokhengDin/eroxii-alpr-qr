b6b1cb1d1ad4b2d5f6478d1f3d651b5ec35ae709
SokhengDin <din.sokheng08@gmail.com>
2026-08-13 08:30:27 +0700

    add hidden

--- startup.bat
+++ startup.bat
@@ -3,4 +3,4 @@ set "PROJECT_DIR=%~dp0"
 set "PROJECT_DIR=%PROJECT_DIR:~0,-1%"
 
 cd /d "%PROJECT_DIR%"
-"%PROJECT_DIR%\.venv\Scripts\python.exe" "%PROJECT_DIR%\main.py"
+start "" "%PROJECT_DIR%\.venv\Scripts\pythonw.exe" "%PROJECT_DIR%\main.py"
